4e05d275c5fbd11b4e3689c0d9a395d5e4766f14
CamiGrace <camidougal@gmail.com>
2023-01-23 19:25:55 -0700

    Project commit

---
+++ PetDiet/src/main/java/com/promineo/PetDiet/PetDietApplication.java
@@ -0,0 +1,13 @@
+package com.promineo.PetDiet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetDietApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetDietApplication.class, args);
+	}
+
+}
